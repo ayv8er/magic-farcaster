995904df8363ff84a442d57260c9782f386bc051
Richard Oh <richard@magic.link>
2024-06-21 17:02:52 -0500

    update api key

--- src/magic.js
+++ src/magic.js
@@ -1,6 +1,6 @@
 import { Magic } from 'magic-sdk';
 import { FarcasterExtension } from "@magic-ext/farcaster"
 
-export const magic = new Magic("pk_live_8817DB7D59D6EC21", {
+export const magic = new Magic("pk_live_4B3A5777BFD8FAF7", {
   extensions: [new FarcasterExtension()]
 })
